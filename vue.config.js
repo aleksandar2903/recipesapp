8d9b0f05f8b3da9b61f8a9615650258cd65e80db
aleksandar2903 <astojanovic174@gmail.com>
2022-08-10 15:22:02 +0200

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/recipesapp/",
+  publicPath: "recipesapp",
   transpileDependencies: true,
 });
